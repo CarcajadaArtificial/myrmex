1f9358ff846aff04e4802a89b75e37203a04cba1
Oscar Alfonso Guerrero Nuñez <oaguerrero.95@gmail.com>
2023-01-05 13:46:27 -0600

    v0.0.14

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,20 +1,11 @@
 # Changelog
 
-## v0.0.13
+## v0.0.14
 
 ### Added
 
-**Updated the requirements document.**
-[`~/docs/requirements/v0.1.0.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/requirements/v0.1.0.md)
+**Moved menus to its own module.**
+
+[`~/src/main.rs`](/src/main.rs), [`~/src/menus.rs`](/src/menus.rs)
 - Better arrangement of information
 - Updated top navigation and what is a widget.
-
-**Updated documentation plans.**
-[`~/docs/plans/Home.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/plans/Home.md)
-[`~/docs/plans/Notes.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/plans/Notes.md)
-[`~/docs/requirements/Updates.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/requirements/Updates.md)
-- Moved information to their own file.
-
-**Renamed the sky phoenomenon to weather block**
-[`~/docs/plans/blocks/Weather.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/plans/blocks/Weather.md)
-[`~/docs/plans/phenomena/Sky.md`](https://github.com/CarcajadaArtificial/Myrmex/blob/main/docs/plans/phenomena/Sky.md)
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+mod menus;
 use eframe::egui;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("My egui App", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
+    eframe::run_native("Myrmex", native_options, Box::new(|cc| Box::new(MyEguiApp::new(cc))));
 }
 
 #[derive(Default)]
@@ -20,17 +21,10 @@ impl MyEguiApp {
 
 impl eframe::App for MyEguiApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        egui::TopBottomPanel::top("top_panel").show(ctx, |ui| {
-            ui.button("Hello World!").clicked();
-        });
+        menus::render_top_panel(ctx);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Hello World!");
         });
-        egui::TopBottomPanel::bottom("bottom_panel").min_height(12.0).show(ctx, |ui| {
-            ui.with_layout(egui::Layout::right_to_left(egui::Align::TOP), |ui| {
-                ui.add(egui::Hyperlink::from_label_and_url("GitHub", "https://github.com/CarcajadaArtificial/Myrmex"));
-                ui.label("Made by Oscar Alfonso Guerrero");
-            });
-        });
+        menus::render_bottom_panel(ctx);
     }
 }
